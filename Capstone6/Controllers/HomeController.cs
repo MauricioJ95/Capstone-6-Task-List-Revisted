fb6e4d5b1ce1f5c4c6f10864ead76c71459f6544
Ty <Tmcarron7@gmail.com>
2018-11-18 15:08:53 -0500

    Heather added stuff

--- Capstone6/Controllers/HomeController.cs
+++ Capstone6/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Capstone6.Controllers
     public class HomeController : Controller
     {
         public TaskListEntities5 ORM = new TaskListEntities5();
-        
+
         public ActionResult Index()
         {
             ViewBag.ListOfTasks = ORM.Tasksses.ToList<Taskss>();
@@ -46,6 +46,43 @@ namespace Capstone6.Controllers
         public ActionResult AddingTask()
         {
             return View();
+        }
+        public ActionResult TaskDetails(int taskID)
+        {
+            Taskss found = ORM.Tasksses.Find(taskID);
+
+            if (found != null)
+            {
+                ViewBag.Task = found;
+                return View();
+            }
+            return View();
+        }
+
+        public ActionResult SaveItemChanges(Taskss NewTask)
+        {
+
+            Taskss oldTask = ORM.Tasksses.Find(NewTask.TaskID);
+            oldTask.Description = NewTask.Description;
+            oldTask.DueDate = NewTask.DueDate;
+            oldTask.Completion = NewTask.Completion;
+
+            ORM.Entry(oldTask).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteItem(int taskID)
+
+        {
+
+            Taskss ItemToDelete = ORM.Tasksses.Find(taskID);
+
+            ORM.Tasksses.Remove(ItemToDelete);
+
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
